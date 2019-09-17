020c3a0c815bfd358963cd3a0664ff1d4b180ec2
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-17 13:21:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,15 @@ attr_reader :name
   
   def initialize(name)
     @name = name
+  end 
+  
+  def roster(roster)
+    @roster = roster 
     roster = {}
+  end 
+  
+  def roster 
+    @roster
   end 
 
 # ROSTER << grade_levels = [array of student names]
