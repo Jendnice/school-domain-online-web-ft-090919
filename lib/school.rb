b99f13282d2d9cc66d6c97258fd7fbd162d467c2
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-17 13:30:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,10 +15,13 @@ attr_reader :name
   def add_student=(student_name, grade)
     @student_name = student_name
     @grade = grade
-    
+    roster << {:grade => [student_name]]
   end
   
   def add_student
+    @student_name = student_name
+    @grade = grade 
+  end 
     
 
 # ROSTER << grade_levels = [array of student names]
