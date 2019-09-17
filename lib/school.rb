81ff27e7ee481663d498820cfff0ff9b71eb7582
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-17 13:17:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School 
+
+attr_accessor 
+attr_reader :name 
+
+  ROSTER = {}
+  
+  def initialize(name)
+    @name = name
+  end 
+
+# ROSTER << grade_levels = [array of student names]
+  
+#  def add_student(name, grade)
+    
+  
+end
