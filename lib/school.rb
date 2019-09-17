0c8819f5cf60076e4ded3a69dbc037a3d6615648
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-17 13:47:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,8 +15,8 @@ attr_reader :name
   def add_student(student_name, grade)
     @student_name = student_name
     @grade = grade
-     @roster[@grade.to_i] = []
-     @grade.to_i << @student_name
+     @roster[grade.to_i] = []
+     @grade << @student_name
 
   end
   
